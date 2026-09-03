f9b1814e0814a6668843ac29b5f718bc5bafb0fb
SimonThormeyer <simon.thormeyer@wire.com>
2026-09-03 14:19:19 +0200

    feat: add rust analyzer wasm toggle

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -18,4 +18,34 @@ return {
       },
     },
   },
+  {
+    "mrcjkb/rustaceanvim",
+    keys = {
+      {
+        "<leader>cw",
+        function()
+          local client = vim.lsp.get_clients({
+            bufnr = 0,
+            name = "rust-analyzer",
+          })[1]
+
+          if not client then
+            vim.notify("rust-analyzer is not running", vim.log.levels.WARN)
+            return
+          end
+
+          local target = vim.tbl_get(client.config.settings, "rust-analyzer", "cargo", "target")
+
+          if target == "wasm32-unknown-unknown" then
+            vim.cmd.RustAnalyzer("target")
+            vim.notify("rust-analyzer: native target")
+          else
+            vim.cmd.RustAnalyzer("target wasm32-unknown-unknown")
+            vim.notify("rust-analyzer: wasm32-unknown-unknown")
+          end
+        end,
+        desc = "Toggle Rust WASM target",
+      },
+    },
+  },
 }
